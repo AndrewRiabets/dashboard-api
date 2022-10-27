3de61f951cf5effbae40123e4ee2d4ec7f578080
AndrewRiabets <Inferion5@gmail.com>
2022-10-27 13:05:00 +0300

    middleware

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 import express from "express";
+import { userRouter } from ".//users/users.js";
 
 const port = 8000;
 const app = express();
 
+userRouter.use((res, req, next) => {
+  console.log("Обработчик users ");
+  next();
+});
+app.use((res, req, next) => {
+  console.log("Время ", Date.now());
+  next();
+});
+
+app.get("/hello", (req, res) => {
+  // res.send('Привет!');
+  res.end();
+});
+
+app.use("/users", userRouter);
+
+app.use((err, res, req, next) => {
+  console.log(err.message);
+  res.status(500).send(err.message);
+});
+
 app.listen(port, () => {
   console.log(`Сервер запущен на ${port} пору`);
 });
-
-app.get("/hello", (res, req) => {
-  res.send("Hello!");
-});
-
-const cb = (req, res, next) => {
-  console.log("CB");
-  next();
-};
-
-app
-  .route("/user")
-  .get("/hello", (req, res) => {
-    res.send("Привет!");
-  })
-  .post("/hello", (req, res) => {
-    res.send("Привет!");
-  });
