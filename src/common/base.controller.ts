4c8c73a250b17da9ffbae98a35f6db69210bb2ec
AndrewRiabets <Inferion5@gmail.com>
2022-11-01 16:06:52 +0200

    base-controller-done

--- src/common/base.controller.ts
+++ src/common/base.controller.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express'
+import { Response, Router } from 'express'
 import {LoggerService} from '../logger/logger.service'
 import { IControllerRoute } from './route.interface'
 
@@ -11,6 +11,19 @@ export abstract class BaseController {
 
     get router() {
         return this._router
+    }
+
+    public send<T>(res: Response, code: number, message: T) {
+        res.type('application/json')
+        return res.status(code).json(message)
+    }
+
+    public ok<T>(res: Response, message: T) {
+        this.send<T>(res, 200, message)
+    }
+
+    public created(res: Response) {
+        return res.sendStatus(201)
     }
 
     protected bindRoutes(routes: IControllerRoute[]) {
