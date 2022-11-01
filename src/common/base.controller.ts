d267ee91a4022b3603bd9f4afa19ef20fc87ce28
AndrewRiabets <Inferion5@gmail.com>
2022-11-01 15:58:56 +0200

    controller-init

---
+++ src/common/base.controller.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express'
+import {LoggerService} from '../logger/logger.service'
+import { IControllerRoute } from './route.interface'
+
+
+export abstract class BaseController {
+    private readonly _router: Router
+    constructor(private logger: LoggerService) {
+         this._router = Router()
+    }
+
+    get router() {
+        return this._router
+    }
+
+    protected bindRoutes(routes: IControllerRoute[]) {
+        for (const route of routes) {
+            this.logger.log(`[${route.method}] ${route.path}`)
+            this.router[route.method](route.path, route.func)
+        }
+    }
+}
