121686c601527ba46312f76b78ec6f338c631309
AndrewRiabets <Inferion5@gmail.com>
2022-11-03 22:02:27 +0200

    add eslint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,26 +1,26 @@
-import { Request, Response, NextFunction } from 'express'
-import {inject, injectable} from 'inversify'
-import {BaseController} from '../common/base.controller'
-import { HTTPError } from '../errors/http-error.class'
-import { ILogger } from '../logger/logger.interface'
-import { LoggerService } from '../logger/logger.service'
-import { TYPES } from '../types'
-import 'reflect-metadata'
-import { IUserController } from './users.controller.interface'
+import { Request, Response, NextFunction } from 'express';
+import { inject, injectable } from 'inversify';
+import { BaseController } from '../common/base.controller';
+import { HTTPError } from '../errors/http-error.class';
+import { ILogger } from '../logger/logger.interface';
+import { LoggerService } from '../logger/logger.service';
+import { TYPES } from '../types';
+import 'reflect-metadata';
+import { IUserController } from './users.controller.interface';
 
 @injectable()
 export class UserController extends BaseController implements IUserController {
-    constructor(@inject(TYPES.ILogger) private loggerService: ILogger) {
-        super(loggerService)
-        this.bindRoutes([{path: '/register', method: 'post', func: this.register}]),
-        this.bindRoutes([{path: '/login', method: 'post', func: this.login}])
-    }
+	constructor(@inject(TYPES.ILogger) private loggerService: ILogger) {
+		super(loggerService);
+		this.bindRoutes([{ path: '/register', method: 'post', func: this.register }]),
+			this.bindRoutes([{ path: '/login', method: 'post', func: this.login }]);
+	}
 
-    login(req: Request, res: Response, next: NextFunction) {
-        // this.ok(res, 'login')
-        next(new HTTPError(401, 'ошибка авторизации', 'login'))
-    }
-    register(req: Request, res: Response, next: NextFunction) {
-        this.ok(res, 'register')
-    }
+	login(req: Request, res: Response, next: NextFunction): void {
+		// this.ok(res, 'login')
+		next(new HTTPError(401, 'ошибка авторизации', 'login'));
+	}
+	register(req: Request, res: Response, next: NextFunction): void {
+		this.ok(res, 'register');
+	}
 }
