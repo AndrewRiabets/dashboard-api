58710ff1a71113fbf404febad73cfc4225bb161a
AndrewRiabets <Inferion5@gmail.com>
2022-11-07 15:44:30 +0200

    add user repository

--- src/users/user-service.ts
+++ src/users/user-service.ts
@@ -5,15 +5,24 @@ import { IUserService } from './user-service.interface';
 import { User } from './user-entity';
 import { TYPES } from '../types';
 import { IConfigService } from '../config/config.service.interface';
+import { IUsersRepository } from './users.repository.interface';
+import { UserModel } from '@prisma/client';
 
 @injectable()
 export class UserService implements IUserService {
-	constructor(@inject(TYPES.ConfigService) private configSrvice: IConfigService) {}
-	async createUser({ email, name, password }: UserRegisterDto): Promise<User | null> {
+	constructor(
+		@inject(TYPES.ConfigService) private configSrvice: IConfigService,
+		@inject(TYPES.UsersRepository) private usersRepository: IUsersRepository,
+	) {}
+	async createUser({ email, name, password }: UserRegisterDto): Promise<UserModel | null> {
 		const newUser = new User(email, name);
 		const salt = this.configSrvice.get('SALT');
 		await newUser.setPassword(password, Number(salt));
-		return null;
+		const exesteduser = await this.usersRepository.find(email);
+		if (exesteduser) {
+			return null;
+		}
+		return this.usersRepository.create(newUser);
 	}
 
 	async validateUser(dto: UserLoginDto): Promise<boolean> {
